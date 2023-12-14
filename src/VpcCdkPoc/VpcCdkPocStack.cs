8fec433bc6c27757dc64832417168f07f626895a
Bongani Nyoni <b.nyoni1101@gmail.com>
2023-12-14 14:39:12 +0200

    Initial commit

---
+++ src/VpcCdkPoc/VpcCdkPocStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace VpcCdkPoc
+{
+    public class VpcCdkPocStack : Stack
+    {
+        internal VpcCdkPocStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
